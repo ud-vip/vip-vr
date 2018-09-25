9badf3f12250d93219ebba46309a737ae1d1956d
tkitson <tkitson@udel.edu>
2018-09-24 20:43:03 -0400

    created a scene with a basic multiplayer interface with sphere to represent players

---
+++ project-vaporwave/project-vaporwave-unity/Assets/Scenes/SphereMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMover : MonoBehaviour {
+
+	const float SPEED = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")){
+			transform.Translate(SPEED * Vector3.forward * Time.deltaTime);
+		}
+		if (Input.GetKey("down")){
+			transform.Translate(SPEED * Vector3.back * Time.deltaTime);
+		}
+		if (Input.GetKey("left")){
+			transform.Translate(SPEED * Vector3.left * Time.deltaTime);
+		}
+		if (Input.GetKey("right")){
+			transform.Translate(SPEED * Vector3.right * Time.deltaTime);
+		}
+	}
+}
